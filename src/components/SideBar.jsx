cc8b82f329801a04b8fb15317a2584b75a7cbbdf
Tchessi <tchesspreza@yahoo.fr>
2022-12-22 12:44:01 +0100

    Layout creating

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+
+export default function SideBar(){
+	return (
+		<Container>
+			SideBar
+		</Container>
+	);
+}
+
+const Container = styled.div`
+	background-color: black;
+`
--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,11 +1,46 @@
 import React from 'react';
+import styled from "styled-components";
+import Body from './Body';
+import Footer from './Footer';
+import Navbar from './NavBar';
+import SideBar from './SideBar';
 
-const Spotify = () => {
+export default function Spotify() {
 	return (
-		<div>
-			App
+	<Container>
+		<div className="spotify__body">
+			<SideBar />
+			<div className="body">
+				<Navbar />
+				<div className="body__contents">
+					<Body/>
+				</div>
+			</div>
 		</div>
+	<div className="spotify__footer">
+		<Footer/>
+	</div>
+	</Container>
 	);
 }
 
-export default Spotify;
+const Container = styled.div`
+	max-width: 100vw;
+	max-height: 100vh;
+	overflow: hidden;
+	display: grid;
+	grid-template-rows: 85vh 15vh;
+	.spotify__body {
+		display: grid;
+		grid-template-columns: 15vw 85vw;
+		height: 100%;
+		width: 100%;
+		background: linear-gradient(transparent, rgba(0, 0, 0, 1));
+		background-color: rgba(32, 87, 100);
+		.body {
+			width: 100%;
+			width: 100%;
+			overflow: auto;
+		}
+	}
+`;
